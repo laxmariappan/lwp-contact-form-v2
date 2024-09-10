43ce342a8e72efcd9528a66a5eb97e70ff424171
Lax Mariappan <elaxmariappan@gmail.com>
2024-09-10 16:02:40 +0530

    add shortcode and placeholder for the form

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### Branches
 - `main` - Stable version
 - `step-1` - Adds readme file and plugin header for the plugin.
+- `step-2` - Adds shortcode and placeholder for the form.
--- lwp-contact-form.php
+++ lwp-contact-form.php
@@ -8,3 +8,28 @@
  *
  * @package           lwp-contact-form
  */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Adds shortcode for the form.
+ * Shortcodes are easier to use and can be placed anywhere in the content.
+ *
+ * @see https://developer.wordpress.org/reference/functions/add_shortcode/
+ */
+add_shortcode( 'lwp_contact_form', 'wp_learn_render_contact_form' );
+/**
+ * Renders the contact form.
+ *
+ * @return string
+ */
+function wp_learn_render_contact_form() {
+    ob_start();
+    ?>
+    <p>Placeholder for the contact form.</p>
+    <?php
+    return ob_get_clean();
+}

af57b5d143509789fc90d37bacd0db23e586af51
Lax Mariappan <elaxmariappan@gmail.com>
2024-09-10 15:53:58 +0530

    add readme and plugin file

---
+++ README.md
@@ -0,0 +1,5 @@
+## LWP Contact Form
+
+### Branches
+- `main` - Stable version
+- `step-1` - Adds readme file and plugin header for the plugin.
---
+++ lwp-contact-form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       LWP Contact Form
+ * Description:       A simple contact form plugin for WordPress.
+ * Version:           1.0.0
+ * Author:            Lax Mariappan
+ * Text Domain:       lwp-contact-form
+ *
+ * @package           lwp-contact-form
+ */
